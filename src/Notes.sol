199537a0b51cf7fba614846c6b0f00c799674241
leon-do <leondo123@gmail.com>
2025-05-19 21:31:35 -0400

    fmt

--- src/Notes.sol
+++ src/Notes.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED 
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
 contract Notes {
-    mapping (address => string) public notes;
+    mapping(address => string) public notes;
 
     function updateNote(string memory _note) public {
         notes[msg.sender] = _note;
